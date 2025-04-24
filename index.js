7eef33406073b1379a3d0ffc132eb3daeddbc0fe
Puja-1998 <ghodkepooja98@gmail.com>
2025-04-24 20:22:36 +0530

    folder structure done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//Ecommece Project
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) =>{
+    res.send("Hello Puja!");
+});
+
+app.listen(port, (err) =>{
+    if(err){
+        console.log("error while connected to the server");  
+    }
+    console.log(`server is running at the http://localhost:${port}`);
+    
+})
